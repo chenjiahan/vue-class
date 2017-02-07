14d6dee943408f5e03c5dd3ecedf899f89c0a563
陈嘉涵 <chenjiahan@meituan.com>
2017-02-07 14:17:31 +0800

    init commit

---
+++ src/index.ts
@@ -0,0 +1,23 @@
+import ClassDecorator from 'vue-class-component';
+
+export default function (options) {
+    return function (component) {
+
+        // vue class component
+        component = ClassDecorator(options)(component);
+
+        // add render function
+        if (options.template) {
+            component = options.template(component);
+        }
+
+        // add name
+        if (options.name) {
+            Object.defineProperty(component, 'name', {
+                value: options.name
+            });
+        }
+
+        return component;
+    };
+}
